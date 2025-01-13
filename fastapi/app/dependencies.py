b2fcbac814ca2bb0002f08165984063f2c894fa2
Alexis Guimbert <alexisguimbert16@gmail.com>
2025-01-13 13:23:50 +0100

    Init projet BigData M2

---
+++ fastapi/app/dependencies.py
@@ -0,0 +1,15 @@
+def get_db_connection():
+    # Logic to get a database connection
+    pass
+
+def get_elasticsearch_client():
+    # Logic to get an Elasticsearch client
+    pass
+
+def get_kafka_producer():
+    # Logic to get a Kafka producer
+    pass
+
+def get_kafka_consumer():
+    # Logic to get a Kafka consumer
+    pass

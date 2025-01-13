1aa15c798d228c3c1162766ea27f74b175259221
Alexis-42 <alexisguimbert16@gmail.com>
2025-01-13 18:41:45 +0100

    tentative de fix de kafka et ajout d'elasticsearch & kibana

--- pipeline/kafka_producer.py
+++ pipeline/kafka_producer.py
@@ -4,7 +4,7 @@ import time
 
 def create_producer():
     producer = KafkaProducer(
-        bootstrap_servers='kafka:9092',
+        bootstrap_servers=['kafka:9092'],
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
     return producer

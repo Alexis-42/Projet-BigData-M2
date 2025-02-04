4ee5c54fac4a75a0e6b5da20d1c7cb2d6df8d0b5
ArthurBlr <art.boullier@gmail.com>
2025-02-04 14:59:48 +0100

    model fin de vie

--- model/app/main.py
+++ model/app/main.py
@@ -5,6 +5,16 @@ from es_connector import ES_connector
 from models import Repo
 import time
 import uvicorn
+import os
+from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
+
+load_dotenv()
+
+CLE_API_GITHUB = os.getenv('CLE_API_GITHUB')
 
 app = FastAPI()
 
@@ -64,95 +74,85 @@ def store_data(data: dict, index_name: Optional[str] = default_index_name) -> di
 """
 @app.post("/call_llm/")
 def call_llm(prompt: str):
+
     try:
-        return StreamingResponse(your_llm_function(prompt), media_type="text/event-stream")
+        return StreamingResponse(generate_readme_with_LLM(prompt), media_type="text/event-stream")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to generate LLM response: {str(e)}")
 
-def your_llm_function(prompt: str):
-    readme_ex = """
-    ## Welcome to The Build a SAAS App with Flask Course!
-
-    *A video course where we build a real world web application with Flask, Celery,
-    Redis, PostgreSQL, Stripe and Docker.*
-
-    **Full details on the course can be found here:**  
-    [https://buildasaasappwithflask.com](https://buildasaasappwithflask.com/?utm_source=github&utm_medium=bsawf&utm_campaign=readme-top)
-
-    ### Getting started
-
-    You'll need to enable Docker Compose v2 support if you're using Docker
-    Desktop. On native Linux without Docker Desktop you can [install it as a plugin
-    to Docker](https://docs.docker.com/compose/install/linux/). It's been generally
-    available for a while now and is stable. This project uses specific [Docker
-    Compose v2
-    features](https://nickjanetakis.com/blog/optional-depends-on-with-docker-compose-v2-20-2)
-    that only work with Docker Compose v2 2.20.2+.
-
-    ```sh
-    cp .env.example .env
-    docker compose up --build
-    ```
-
-    After everything is up and running, visit http://localhost:8000.
-
-    Did you receive a `depends_on` "Additional property required is not allowed"
-    error? Please update to at least Docker Compose v2.20.2+ or Docker Desktop
-    4.22.0+.
-
-    Did you receive an error about a port being in use? Chances are it's because
-    something on your machine is already running on port 8000. Check out the docs
-    in the `.env` file for the `DOCKER_WEB_PORT_FORWARD` variable to fix this.
-
-    Did you receive a permission denied error? Chances are you're running native
-    Linux and your `uid:gid` aren't `1000:1000` (you can verify this by running
-    `id`). Check out the docs in the `.env` file to customize the `UID` and `GID`
-    variables to fix this.
-
-    ### How does this source code differ than what's in the course?
-
-    In the course we build up a 4,000+ line Flask application in 15 stages while
-    I'm at your side explaining my thought process along the way. You will get to
-    see the source code grow from a single `app.py` file to a large code base that
-    spans across dozens of files and folders.
-
-    #### This repo includes up to the 6th stage. By this point in the code base, you'll be introduced to concepts such as:
-
-    - Using Docker to "Dockerize" a multi-service Flask app
-    - Using Flask extensions
-    - Flask blueprints
-    - Jinja templates
-    - Working with forms
-    - Sending e-mails through Celery
-    - Testing and analyzing your code base
-
-    #### The rest of the course covers topics such as:
-
-    - A crash course on Docker and Docker Compose (including multi-stage builds)
-    - Going over the application's architecture and tech choices
-    - Creating a full blown user management system
-    - Creating a custom admin dashboard
-    - Logging, middleware and error handling
-    - Using Click to create custom CLI commands
-    - Accepting recurring credit card payments with Stripe
-    - Building up a dice game called "Snake Eyes"
-    - Responding with JSON from Flask and creating AJAX requests
-    - Processing microtransaction payments with Stripe
-    - Dealing with database migrations
-    - Converting your app to support multiple languages (i18n)
-    - A crash course on Webpack, ES6 JavaScript and SCSS
-
-    **By the time you finish the course, you'll have all the confidence you need to
-    build a large web application with Flask**.
-
+def generate_readme_with_LLM(input_text: str, model_name: str = "google/flan-t5-large"):
     """
-    response = ["hello ", "i am a custom LLM ", "i am generating text", " this is the last part of the response","### README START ###", readme_ex, "### README END ###"]
-    try:
-        for i in range(len(response)):
-            time.sleep(1)
-            yield response[i]
-    except GeneratorExit:
-        print("Client disconnected")
+    Generate a README using the given LLM.
+    
+    Args:
+        input_text (str): LLM input string.
+        model_name (str): LLM model name to use. Defaults to "google/flan-t5-large".
+    
+    Returns:
+        str: generated README.
+    """
+
+        
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    input_text = prepare_input_for_LLM(input_text)
+
+    inputs = tokenizer(input_text, return_tensors="pt", truncation=True, max_length=500)
+    outputs = model.generate(inputs["input_ids"], max_length=1024, num_beams=5, early_stopping=True)
+    generated_readme = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return [generated_readme]
+
+# Exemple structuré de README en Markdown
+EXAMPLE_README = """
+    # Example Project
+
+    ## Description
+    This is a sample project demonstrating best practices for structuring a README file. It includes sections that provide all necessary details to understand and use the project effectively.
+
+    ## Features
+    - List of features here
+    - Add any additional functionality
+
+    ## Technologies Used
+    - Technology 1
+    - Technology 2
+
+    ## Installation
+    1. Clone the repository:
+    ```bash
+    git clone https://github.com/username/example-project.git
+"""
+
+def prepare_input_for_LLM(user_query,k=5):
+    """
+    Prepare the input for the LLM to generate a README based on the user query and related README files.
+
+    Args:
+        user_query (str): User query.
+        readmes (list): List of related README files.
+        k (int): Number of related README files to include. Defaults to 5.
+
+    Returns:
+        str: Input text for the LLM.
+    """
+    # Rechercher les documents les plus similaires
+    k = 5  # Nombre de documents à récupérer
+
+    top_readmes = search(q = user_query, number_of_result=k)
+
+    # Préparer l'entrée pour le modèle
+    input_text = (
+        f"Generate a README based on the following topic: {user_query}.\n\n"
+        f"Here is an example of a well-structured README on which you can base the structure of your response:\n\n{EXAMPLE_README}\n\n"
+        f"Here are some related README files:\n\n" +
+        "\n\n".join([f"Repository: {repo['name']}\nREADME:\n{repo['readme']}" for repo in top_readmes]) + "\n\n"
+        f"Please generate a complete and formatted README in English for my project based on the example for the structure and followed by the related README files, ready to be copied into a README.md file.\n"
+        f"Do not include this prompt in the generated README."
+        f"Replace by \\n the line breaks in the generated README."
+    )
+
+    return input_text
+
    
 @app.get("/llm_list")
 def llm_list():
